bd8c46d79ff291fc89f85f7b9cadb55f0b647a06
Giovanni Luca Izzi <gizzi@redhat.com>
2024-02-09 22:56:36 +0100

    add tcp-server

---
+++ tcp-server/tcp_server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
+}
+
+func sendResponse(conn net.Conn, msg []byte) {
+	_, err := conn.Write(msg)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	conn.Close()
+}
+
+func main() {
+	port := "10080"
+
+	fmt.Println("Listening on", port)
+
+	/* Hostname */
+	hostname, err := os.Hostname()
+	fmt.Println("Hostname=" + hostname)
+	checkError(err)
+
+	/* Lets prepare a address at any address at port 10001*/
+	//serverAddr, err := net.ResolveTCPAddr("tcp", ":"+port)
+	serverAddr := ":" + port
+	checkError(err)
+
+	/* Now listen at selected port */
+	listener, err := net.Listen("tcp", serverAddr)
+	checkError(err)
+	defer listener.Close()
+
+	msg := []byte(hostname)
+	//buf := make([]byte, 2048)
+
+	for {
+		tcpconn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error while accepting: ", err)
+			continue
+		}
+
+		reqString, err := bufio.NewReader(tcpconn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error while reading from remote: ", err)
+			continue
+		}
+
+		fmt.Println("Received ", reqString, " from ", tcpconn.RemoteAddr().String())
+
+		go sendResponse(tcpconn, msg)
+
+	}
+}
